167d759c6e0752cb65124f1c4445c3f46e3ebd36
Wenderson José <wenderson.teles@icloud.com>
2025-12-03 18:12:27 -0300

    feat:"Adicionado a pasta getset"

---
+++ POO/getset/Main.java
@@ -0,0 +1,12 @@
+package POO.getset;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa p = new Pessoa();
+
+        p.setNome("Felipe");
+
+        System.out.println(p.getNome());
+    }
+}
---
+++ POO/getset/Pessoa.java
@@ -0,0 +1,14 @@
+package POO.getset;
+
+public class Pessoa {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
