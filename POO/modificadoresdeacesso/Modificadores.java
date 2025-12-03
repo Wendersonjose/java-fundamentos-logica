a99969a8074ae3933341772d11f827eeabdb2d98
Wenderson José <wenderson.teles@icloud.com>
2025-12-03 19:30:56 -0300

    feat: pastas adicionadas

---
+++ POO/modificadoresdeacesso/Modificadores.java
@@ -0,0 +1,20 @@
+
+
+public class Modificadores {
+    
+}
+/* 
+-> Default: Qunado nao e especificado o modificador de acesso. 
+Atributos e metodos estarao disponiveis apenas em arquivos no mesmo paciote. 
+Exemplo:String nome; | void mensagem(){}
+
+-> Public:Para deixar atributyos e metodos visiveis sem restricoes.
+Exemplo: public String nome; | public void mensagem(){}
+
+-> Private: Atributos e metodos visiveis apenas na classe de criacao.
+Exemplo: private String nome; | private void mensagem(){}
+
+-> Protected: Atributos e metodos visiveis na classe de criacao ou herdadas.
+Exemplo: public String nome; | protected void mensagem(){}
+
+*/
