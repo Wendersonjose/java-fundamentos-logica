b00059aca3a9b420d6f6cb200c9b53b121b95413
Wenderson José <wenderson.teles@icloud.com>
2026-01-04 12:19:09 -0300

    feat: adicao metodo super

---
+++ POO/estatico/Classe.java
@@ -0,0 +1,17 @@
+package POO.estatico;
+
+public class Classe {
+
+    //Atributo
+    public static String nome = "Livia";
+
+    // Metodo
+
+    public static void somar(int n1, int n2 ){
+        System.out.println(n1+n2);
+
+    }
+
+    }
+    
+
---
+++ POO/estatico/Main.java
@@ -0,0 +1,14 @@
+package POO.estatico;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+   
+
+        System.out.println(Classe.nome);   
+        
+        Classe.somar(5, 9);
+    }
+    
+}
