a99940b327b1230e90f0b09ec1b45d5dd351802b
Wenderson José <wenderson.teles@icloud.com>
2026-01-04 10:19:23 -0300

    feat: adiciona exemplo de herança em POO

---
+++ POO/heranca/Colaborador.java
@@ -0,0 +1,21 @@
+public class Colaborador extends Pessoa {
+
+    private String cargo;
+    private double salario;
+
+    public Colaborador(String nome, int idade, String cargo, double salario) {
+        super(nome, idade); // 👈 herança correta
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    protected void mensagemColaborador() {
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Salário: " + salario);
+    }
+
+    public void exibirDados() {
+        mensagemPessoa();
+        mensagemColaborador();
+    }
+}
---
+++ POO/heranca/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Colaborador c = new Colaborador(
+            "Wenderson",
+            38,
+            "Desenvolvedor de Sistema",
+            5000.0
+        );
+
+        c.exibirDados();
+    }
+}
